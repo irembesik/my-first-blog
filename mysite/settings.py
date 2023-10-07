713921defdf1d9019231c942320a06b210bd8722
irembesik <s.irem.besik@gmail.com>
2023-10-07 17:23:50 +0200

    Added image

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = BASE_DIR / 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
